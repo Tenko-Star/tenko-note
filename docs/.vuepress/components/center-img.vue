345b917b15cf6064b943fe4ea0fb8f322bc4e044
Tenko_ <admin@tenko.life>
2023-07-30 21:52:09 +0800

    update doc

---
+++ docs/.vuepress/components/center-img.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <img :src="imgUrl" :alt="alt">
+        <div v-if="text" class="tip">
+            <a v-if="link" :href="link" target="_blank" rel="noopener noreferrer">{{ text }}</a>
+            <span v-else>{{ text }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        img: {
+            type: String,
+            require: true,
+            default: ''
+        },
+        alt: {
+            type: String,
+            require: false,
+            default: 'image'
+        },
+        link: {
+            type: String,
+            require: false,
+            default: ''
+        },
+        text: {
+            type: String,
+            require: false,
+            default: ''
+        }
+    },
+    data() {
+        return {
+            imgUrl: ''
+        }
+    },
+    mounted() {
+        import('/imgs/'+this.img).then(res => {
+            console.log(res.default)
+            this.imgUrl = res.default
+        })
+    }
+}
+</script>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.container .tip {
+    margin-top: 10px;
+    width: 80%;
+    text-align: center;
+}
+</style>
